23d6879ab638df0f39e038397f9c212372388fa0
rbrock44 <rbrock444@gmail.com>
2025-08-14 09:32:21 -0500

    update javaloops2 with hackerrank web portal code lines

--- src/main/java/org/exercises/JavaLoopsII.java
+++ src/main/java/org/exercises/JavaLoopsII.java
@@ -5,16 +5,23 @@ import java.util.*;
 
 public class JavaLoopsII {
 
+    // commented out lines are for the HackerRank web portal
+    // lines with comments at the end are specific to my project
     public String[] main(String[] args) {
-        int q = Integer.parseInt(args[0]);
+//        Scanner scanner = new Scanner(System.in);
+//        int q = scanner.nextInt();
+        int q = Integer.parseInt(args[0]); //
 
-        String[] queries = new String[q];
+        String[] queries = new String[q]; //
         int argsIndex = 1;
         for (int i = 0; i < q; i++) {
             String[] queryArgs = args[argsIndex].split(" ");
-            int a = Integer.parseInt(queryArgs[0]);
-            int b = Integer.parseInt(queryArgs[1]);
-            int n = Integer.parseInt(queryArgs[2]);
+            int a = Integer.parseInt(queryArgs[0]); //
+            int b = Integer.parseInt(queryArgs[1]); //
+            int n = Integer.parseInt(queryArgs[2]); //
+//            int a = scanner.nextInt();
+//            int b = scanner.nextInt();
+//            int n = scanner.nextInt();
 
             StringBuilder query = new StringBuilder();
 
@@ -25,9 +32,11 @@ public class JavaLoopsII {
                 query.append(" ").append(sum);
             }
 
-            queries[i] = query.toString().trim();
+            queries[i] = query.toString().trim(); //
+//            System.out.println(query.toString().trim());
             argsIndex++;
         }
-        return queries;
+//        scanner.close();
+        return queries; //
     }
 }
