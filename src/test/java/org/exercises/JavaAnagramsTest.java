fde5adbe331992c4613c05033ba69facde90629e
rbrock44 <rbrock444@gmail.com>
2025-08-14 14:38:17 -0500

    add JavaAnagrams

---
+++ src/test/java/org/exercises/JavaAnagramsTest.java
@@ -0,0 +1,27 @@
+package org.exercises;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class JavaAnagramsTest {
+    @ParameterizedTest
+    @CsvSource({
+        "anagram,margana,Anagrams",
+        "cat,ACT,Anagrams",
+        "Hello,hello,Anagrams",
+        "anagram,argana,Not Anagrams",
+        "ab,abXY,Not Anagrams",
+    })
+    void worksAsExpected(String a, String b, String expected) {
+        String[] args = Stream.concat(
+            Stream.of(a),
+            Stream.of(b)
+        ).toArray(String[]::new);
+
+        assertEquals(expected, JavaAnagrams.main(args));
+    }
+}
