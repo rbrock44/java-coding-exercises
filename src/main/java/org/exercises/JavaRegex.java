e9794f15f2a89c6fab9f0a0af6a6613c2c0903a9
rbrock44 <rbrock444@gmail.com>
2025-08-20 20:38:06 -0500

    add JavaRegex

---
+++ src/main/java/org/exercises/JavaRegex.java
@@ -0,0 +1,28 @@
+package org.exercises;
+
+import java.util.Scanner;
+
+class MyRegex {
+    public String pattern = "([01]?\\d{1,2}|2[0-4]\\d|25[0-5])"+"."+
+        "([01]?\\d{1,2}|2[0-4]\\d|25[0-5])"+"."+
+        "([01]?\\d{1,2}|2[0-4]\\d|25[0-5])"+"."+
+        "([01]?\\d{1,2}|2[0-4]\\d|25[0-5])";;
+}
+
+public class JavaRegex {
+    public static Boolean main(String[] args) {
+        String ip = args[0].toUpperCase();
+
+        return ip.matches(new MyRegex().pattern);
+    }
+
+    // HackerRank web portal solution
+    public static void main2(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String ip = in.next().toUpperCase();
+
+        System.out.println(ip.matches(new MyRegex().pattern));
+    }
+
+
+}
---
+++ src/test/java/org/exercises/JavaRegexTest.java
@@ -0,0 +1,25 @@
+package org.exercises;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JavaRegexTest {
+    @ParameterizedTest
+    @CsvSource({
+        "000.12.12.034,true",
+        "121.234.12.12,true",
+        "23.45.12.56,true",
+        "00.12.123.123123.123,false",
+        "122.23,false",
+        "Hello.IP,false",
+    })
+    void worksAsExpected(String ip, Boolean expected) {
+        String[] args = Stream.of(ip).toArray(String[]::new);
+
+        assertEquals(expected, JavaRegex.main(args));
+    }
+}
